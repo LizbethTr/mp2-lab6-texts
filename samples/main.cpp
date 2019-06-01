78e83e8bf92f604cb88d7a4bcb72426ff8d71d0f
LizbethTr <42984332+LizbethTr@users.noreply.github.com>
2019-06-02 00:33:20 +0300

    Revert "B69 (#1)"
    This reverts commit caaa72bca2f8c34d1bfdc52c642f7b0f59c7047e.

--- samples/main.cpp
+++ samples/main.cpp
@@ -5,31 +5,10 @@
 //
 
 #include <iostream>
-#include "TText.h"
 
 using namespace std;
 
 int main()
 {
-
-	TTextLink::InitMemSystem();
-	TText txt;
-	txt.SetLine("1 Chapter");
-	txt.InsDownLine("1.1 Chapter");
-	txt.GoDownLink();
-	txt.InsNextLine("1.2 Chapter");
-	txt.GoPrevLink();
-
-	txt.InsNextLine("2 Chapter");
-	txt.GoNextLink();
-
-	txt.InsNextLine("3 Chapter");
-	txt.GoNextLink();
-	txt.InsDownLine("3.1 Chapter");
-	txt.GoDownLink();
-	txt.InsDownLine("3.1.1 Chapter");
-
-	txt.Print();
-
-	return 0;
+  return 0;
 }
